b0c0d119a77ed00ea7f2d563444bc84c63ed86a2
derIgel418 <ignaz.poth@evgym.at>
2026-09-09 15:34:38 +0200

    fixed filename

--- LEVEL1/deparser.py
+++ LEVEL1/deparser.py
@@ -1,7 +1,7 @@
 l = []
 
-def makefile(lines):
-    with open("Out/level1_1_small.out", "a") as f:
+def makefile(lines, filename):
+    with open(f"Out/{filename}.out", "a") as f:
         #f.write(str() + "\n")
         for l in lines:
             f.write(l+ "\n")
