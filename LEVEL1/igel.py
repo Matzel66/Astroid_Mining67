60552d6728fc3c66dc4223967d37767229ad3ecb
derIgel418 <ignaz.poth@evgym.at>
2026-09-09 15:25:17 +0200

    fixed something

--- LEVEL1/igel.py
+++ LEVEL1/igel.py
@@ -5,5 +5,5 @@ def print_asteroids(width, height):
     for i in range(height):
         lines.append('#' + ':'*(width) + '#')
     lines.append('#'*(width+2))
-
-print_asteroids(10, 4)
+    return lines
+#print_asteroids(10, 4)
--- LEVEL1/main.py
+++ LEVEL1/main.py
@@ -1,5 +1,6 @@
 from igel import *
 from Parser import *
+from deparser import *
 
 file_name = "Files/level1_0_example.in"
 
@@ -7,4 +8,4 @@ data2 = parse(file_name)
 print(data2)
 
 for values in data2:
-    print_asteroids(int(values[0]), int(values[1]))
+    makefile(print_asteroids(int(values[0]), int(values[1])))
