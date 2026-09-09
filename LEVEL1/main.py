f1f36528251e9e0c28f9df40387fe9ac1c5eca40
derIgel418 <ignaz.poth@evgym.at>
2026-09-09 14:53:37 +0200

    solved level 1

--- LEVEL1/main.py
+++ LEVEL1/main.py
@@ -1,10 +1,11 @@
 def print_asteroids(width, height):
-    print('#'*width)
+    print('#'*(width+2))
     
     for i in range(height):
-        print('#' + ':'*(width-2) + '#')
-        
-print_asteroids(10, 4)
+        print('#' + ':'*(width) + '#')
+    print('#'*(width+2))
+    print()
+#print_asteroids(10, 4)
 
 
 
@@ -13,3 +14,6 @@ with open("Files/level1_0_example.in", "r") as f:
     data2 = data1[1:]
 
 print(data2)
+
+for values in data2:
+    print_asteroids(int(values[0]), int(values[1]))
